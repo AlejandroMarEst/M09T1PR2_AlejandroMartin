84221f29c9b68b7affbb62cd2fe3028fe113f536
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-04-21 02:50:51 +0200

    feature(api): Created class for game votes Closes #3

---
+++ M09T1PR2API_AlejandroMartin/Model/GameVoting.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace M09T1PR2API_AlejandroMartin.Model
+{
+    public class GameVoting
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public int UserId { get; set; }
+    }
+}
