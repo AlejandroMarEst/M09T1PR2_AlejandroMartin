07355d11e37d1ef9ed5dff242cc942edd56462f5
AlejandroMarEst <alejandro.martin.7e8@itb.cat>
2025-04-21 17:18:58 +0200

    feature(client): Added ability to vote for games Closes #20

---
+++ M09T1PR2Razor_AlejandroMartin/DTOs/GameVotingDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace M09T1PR2Razor_AlejandroMartin.DTOs
+{
+    public class GameVotingDTO
+    {
+        public int Id { get; set; }
+        public int GameId { get; set; }
+        public string UserId { get; set; }
+    }
+}
--- M09T1PR2Razor_AlejandroMartin/Pages/GameInfo.cshtml.cs
+++ M09T1PR2Razor_AlejandroMartin/Pages/GameInfo.cshtml.cs
@@ -1,6 +1,7 @@
 using M09T1PR2Razor_AlejandroMartin.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.Http.Headers;
 using System.Text.Json;
 
 namespace M09T1PR2Razor_AlejandroMartin.Pages
@@ -9,6 +10,9 @@ namespace M09T1PR2Razor_AlejandroMartin.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly IHttpClientFactory _httpClientFactory;
+        public bool LoggedIn { get; set; }
+        public bool Voted { get; set; } = false;
+        public int Votes { get; set; }
 
         public GameDTO Game { get; set; } = new GameDTO();
 
@@ -18,19 +22,20 @@ namespace M09T1PR2Razor_AlejandroMartin.Pages
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task OnGetGame(int id)
+        public async Task OnGet(int id)
         {
+            LoggedIn = Tools.TokenHelper.IsTokenSession(HttpContext.Session.GetString("AuthToken"));
             var client = _httpClientFactory.CreateClient("GameApi");
             try
             {
-                var response = await client.GetAsync($"api/Game/{id}");
-                if (response == null || !response.IsSuccessStatusCode)
+                var gameInfo = await client.GetAsync($"api/Game/{id}");
+                if (gameInfo == null || !gameInfo.IsSuccessStatusCode)
                 {
-                    _logger.LogError("Error loading Game Info");
+                    _logger.LogError("Error loading Game info");
                 }
                 else
                 {
-                    var json = await response.Content.ReadAsStringAsync();
+                    var json = await gameInfo.Content.ReadAsStringAsync();
                     Game = JsonSerializer.Deserialize<GameDTO>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
                 }
             }
@@ -38,6 +43,43 @@ namespace M09T1PR2Razor_AlejandroMartin.Pages
             {
                 _logger.LogError(ex.Message);
             }
+            var votes = await client.GetAsync($"api/GameVotes/GetVotes/{id}");
+            Votes = Convert.ToInt32(await votes.Content.ReadAsStringAsync());
+            if (LoggedIn)
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("AuthToken"));
+                var response = await client.GetAsync("api/GameVotes/GetVotes");
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    List<GameVotingDTO> gameVotes = JsonSerializer.Deserialize<List<GameVotingDTO>>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                    GameVotingDTO? gameVote = gameVotes.FirstOrDefault(g => g.GameId == id);
+                    if (gameVote != null)
+                    {
+                        Voted = true;
+                    }
+                    else
+                    {
+                        Voted = false;
+                    }
+                }
+            }
+        }
+
+        public async Task<IActionResult> OnPostVote(int gameId)
+        {
+            var client = _httpClientFactory.CreateClient("GameApi");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("AuthToken"));
+            var response = await client.PostAsJsonAsync("api/GameVotes/Vote", gameId);
+            return RedirectToPage("GameInfo", new { id = gameId });
+        }
+
+        public async Task<IActionResult> OnPostUnVote(int gameId)
+        {
+            var client = _httpClientFactory.CreateClient("GameApi");
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", HttpContext.Session.GetString("AuthToken"));
+            var response = await client.PostAsJsonAsync($"api/GameVotes/Unvote", gameId);
+            return RedirectToPage("GameInfo", new { id = gameId });
         }
     }
 }
